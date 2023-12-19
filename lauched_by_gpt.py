6efd026d145ae1fffa0eb06a4f23ffbb7b62ec74
unknown <zionyow@gmail.com>
2023-12-19 23:29:32 +0800

    Run through vision

--- lauched_by_gpt.py
+++ lauched_by_gpt.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import openai
 import typer
+from random import randint
+import time
 
 app = typer.Typer()
 
@@ -19,9 +21,6 @@ class Bill(object):
     def __init__(
         self,
         row:pd.core.series.Series,
-        # txt_eles:str,
-        # head_txts:str,
-        # tail_txts:str
         ):
         '''
         Categorization by row.
@@ -31,31 +30,45 @@ class Bill(object):
         self.head_txts = '这是我的交易记录:' 
         self.txts = f'在{self.txt_eles[0]}, 我花费了{self.txt_eles[-1] }元人民币向{self.txt_eles[1]} 购买了{self.txt_eles[2]}。'
         self.tail_txts = \
-        '''请帮我将这笔订单记录分类, 在以下类别 ['必要性食品','非必要性食品','交通','一般耐用品','电子耐用品','订阅类信息服务或娱乐产品','线下娱乐或休闲'] 中，上面这个订单属于哪一类?只需要回答所属类别名称。'''
-        
+        '''请帮我将这笔订单记录分类（只返回类别名称，如果难以判断则归类为“其他”），在以下类别 [食品,交通,耐用品,订阅类信息服务或娱乐产品,线下娱乐或休闲,理财] 中，上面这个订单属于哪一类?'''
         self.asking_texts = self.head_txts + self.txts + self.tail_txts
     
     # classifier
     def classify_expense(self):
         # try:
         response = openai.ChatCompletion.create(
-            model='gpt-3.5-turbo',#"gpt-4.0",
-            prompt=[{"role": "system", "content": "你是一个根据订单记录对订单进行分类的智能助力。"},
+            model='gpt-4-1106-preview',
+            messages=[{"role": "system", "content": "你是一个根据订单记录对订单进行分类的智能助力，你言简意赅。你擅长结合交易对方的名称，交易发生的时间，交易金额判断支出类型。"},
                     {"role": "user", "content": self.asking_texts}],
-            max_tokens=128
+            max_tokens=16
         )
         return response['choices'][0]['message']['content']
-        # except Exception as e:
-        #     print(f"Error: {e}")
-        #     return "Categorization Failed!."
 
 @app.command()
 def classfying():
-    cates = {}
-    for idx, rcd in bill_df.iterrows():
-        one_bill = Bill(rcd)
-    print(one_bill.classify_expense())
+    # read bills record
+    bill_df = pd.read_csv(r'\Dataloader\his\History_records_alipay.csv', index_col=0)
 
+    # copy a datetime column
+    bill_df['时间'] =bill_df['交易时间'].copy() 
+    Bills_df = bill_df.set_index('时间').sort_index()
+
+    # dict to accept the result
+    cates = {}
+    for idx, rcd in tqdm(Bills_df.iterrows()):
+        if idx in cates.keys():
+            continue
+        
+        one_bill = Bill(rcd)
+        cates[idx] = one_bill.classify_expense()
+        print(rcd['商品'],cates[idx])
+        time.sleep(randint(1,2))
+
+    # concat the category
+    Bills_df['cate'] = pd.Series(cates)
+
+    # output
+    Bills_df.to_excel('CateFinishedBill.xlsx')
 
 if __name__ ==  '__main__':
     app()
